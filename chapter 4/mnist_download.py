c3941e602f663575bbc54c5804930174cb8f425e
kimnanhee <kimnanhee0225@gmail.com>
2021-03-23 19:24:31 +0900

    [Add] mnist_tocsv.py

--- chapter 4/mnist_download.py
+++ chapter 4/mnist_download.py
@@ -4,10 +4,10 @@ import gzip, os, os.path
 savepath = "./mnist"
 baseurl = "http://yann.lecun.com/exdb/mnist"
 files = [
-    "train-images-idx3-ubyte.gz",
-    "train-labels-idx1-ubyte.gz",
+    "t10k-labels-idx1-ubyte.gz",
     "t10k-images-idx3-ubyte.gz",
-    "t10k-labels-idx1-ubyte.gz"
+    "train-labels-idx1-ubyte.gz",
+    "train-images-idx3-ubyte.gz",
 ]
 
 # 다운로드
@@ -20,14 +20,17 @@ for f in files:
         req.urlretrieve(url, loc)
 
 # GZip 압축 해제
-for f in files:
-    gz_file = savepath + "/" + f
-    raw_file = savepath + "/" + f.replace(".gz", "")
-    print("gzip : ", f, )
-    with gzip.open(gz_file, "rb") as fp:
-        body = fp.read()
-        with open(raw_file, "wb") as w:
-            w.write(body)
-        fp.close()
-
-print("ok")
+try:
+    for f in files:
+        gz_file = savepath + "/" + f
+        raw_file = savepath + "/" + f.replace(".gz", "")
+        print("gzip : ", f, )
+        with gzip.open(gz_file, "rb") as fp:
+            body = fp.read()
+            if len(body)==0:
+                print("error")
+            with open(raw_file, "wb") as w:
+                w.write(body)
+        print("ok")
+except:
+    pass
