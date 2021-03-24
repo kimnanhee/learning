b00ec7ab3a938800c423b56a1618cba74f073b69
kimnanhee <kimnanhee0225@gmail.com>
2021-03-24 22:55:17 +0900

    [Add] lang_train_save.py, freq.pkl

--- chapter 4/lang_train_save.py
+++ chapter 4/lang_train_save.py
@@ -1,9 +1,16 @@
 from sklearn import svm
-from sklearn.externals import joblib
+import joblib
 import json
 
 # JSON 파일의 데이터 읽어오기
 with open("./lang/freq.json", "r", encoding="utf-8") as fp:
     d = json.load(fp)
-    print(d)
     data = d[0]
+
+# 데이터 학습하기
+clf = svm.SVC()
+clf.fit(data["freqs"], data["labels"])
+
+# 학습 데이터 저장하기
+joblib.dump(clf, "./lang/freq.pkl")
+print("ok")
