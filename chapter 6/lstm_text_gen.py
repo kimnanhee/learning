cecb0d969b6f82ff072306e619dd1e50636834c8
kimnanhee <kimnanhee0225@gmail.com>
2021-04-13 23:03:18 +0900

    [Modify] lstm_text_gen.py

--- chapter 6/lstm_text_gen.py
+++ chapter 6/lstm_text_gen.py
@@ -19,3 +19,71 @@ chars = sorted(list(set(text)))
 print("사용되고 있는 문자의 수 :", len(chars))
 char_indices = dict((c, i) for i, c in enumerate(chars)) # 문자 -> ID
 indices_char = dict((i, c) for i, c in enumerate(chars)) # ID -> 문자
+
+# 텍스트를 maxlen개의 문자로 자르고 다음에 오는 문자 등록하기
+maxlen = 20
+step = 3
+sentences = []
+next_chars = []
+for i in range(0, len(text) - maxlen, step):
+    sentences.append(text[i : i+maxlen])
+    next_chars.append(text[i+maxlen])
+print("학습할 구문의 수 :", len(sentences))
+print("텍스트를 ID 벡터로 변환합니다...")
+X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+Y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+for i, sentences in enumerate(sentences):
+    for t, char in enumerate(sentences):
+        X[i, t, char_indices[char]] = 1
+    Y[i, char_indices[next_chars[i]]] = 1
+
+# LSTM 모델 구축하기
+print("모델을 구축합니다...")
+model = Sequential()
+model.add(LSTM(128, input_shape=(maxlen, len(chars))))
+model.add(Dense(len(chars)))
+model.add(Activation("softmax"))
+optimizer = RMSprop(lr=0.01)
+model.compile(loss="categorical_crossentropy", optimizer=optimizer)
+
+# 후보를 배열에서 꺼내기
+def sample(preds, temperature=1.0):
+    preds = np.array(preds).astype("float64")
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+# 학습시키고 텍스트 생성하기 반복
+for iteration in range(1, 60):
+    print()
+    print("-" * 50)
+    print("반복 :", iteration)
+    model.fit(X, Y, batch_size=128, epochs=1)
+    # 임의의 시작 텍스트 선택하기
+    start_index = random.randint(0, len(text) - maxlen - 1)
+    # 다양한 다양성의 문장 생성
+    for diverity in [0.2, 0.5, 1.0, 1.2]:
+        print()
+        print("--- 다양성 :", diverity)
+        generated = ""
+        sentence = text[start_index: start_index + maxlen]
+        generated += sentence
+        print("--- 시드 :", sentence)
+        sys.stdout.write(generated)
+        # 시드를 기반으로 텍스트 자동 생성
+        for i in range(400):
+            x = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x[0, t, char_indices[char]] = 1.0
+            # 다음에 올 문자 예측하기
+            preds = model.predict(x, verbose=0)[0]
+            next_index = sample(preds, diverity)
+            next_char = indices_char[next_index]
+            # 출력하기
+            generated += next_char
+            sentence = sentence[1:] + next_char
+            sys.stdout.write(next_char)
+            sys.stdout.flush()
+        print()
