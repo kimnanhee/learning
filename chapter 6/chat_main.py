efbd24afb7e9e0b67f5676cb51e8ea9aea920225
kimnanhee <kimnanhee0225@gmail.com>
2021-04-20 20:28:17 +0900

    [Modify] chat_main.py

--- chapter 6/chat_main.py
+++ chapter 6/chat_main.py
@@ -1,38 +1,42 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QLabel, QWidget, QMainWindow
 from chat import *
 from chat_engine import make_reply
 
-class Chat():
+class Chat(QMainWindow):
     def __init__(self):
-        self.text_list = []
+        super().__init__()
+        self.text_list = [" " for i in range(4)]
+
+        ui = Ui_MainWindow()
+        ui.setupUi(self)
+
         self.label_list = [ui.label_user_1, ui.label_bot_1, ui.label_user_2, ui.label_bot_2, ui.label_user_3, ui.label_bot_3]
         
         ui.pushButton.clicked.connect(lambda: self.send())
+        self.ui = ui
+        # self.setMouseTracking(True)
+
+    # def wheelEvent(self, e): # e ; QWheelEvent 
+    #     print('wheel')
+    #     print('(%d %d)' % (e.angleDelta().x(), e.angleDelta().y()))
 
     def send(self):
-        text = ui.lineEdit.text()
-        if text == "":
-            return
-
+        text = self.ui.lineEdit.text()
+        if text == "": return
+        print(text)
         self.text_list.append(text)
         self.text_list.append(make_reply(text))
- 
-        if len(self.text_list) > 6:
-            for i, text in enumerate(self.text_list[-6:]):
-                self.label_list[i].setText(text)
-                print(i, text)
 
-        ui.lineEdit.setText("")
+        for i, text in enumerate(self.text_list[-6:]):
+            self.label_list[i].setText(text)
+            print(i, text)
+
+        self.ui.lineEdit.setText("")
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-
-    chat = Chat()
-
-    MainWindow.show()
+    main = Chat()
+    main.show()
     sys.exit(app.exec_())
